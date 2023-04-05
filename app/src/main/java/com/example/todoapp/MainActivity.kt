5b990a616546ec8c0fd4b08ef9801e158c60a289
Navojith <navojith22@gmail.com>
2023-04-05 13:05:09 +0530

    without db

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.adapters.TodoAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.rvTodoList)
+        val adapter = TodoAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/example/todoapp/adapters/TodoAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.todoapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.R
+
+class TodoAdapter:RecyclerView.Adapter<TodoAdapter.ViewHolder>() {
+    class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val cbTodo: CheckBox
+        val ivDelete: ImageView
+        init {
+            cbTodo = view.findViewById(R.id.cbTodo)
+            ivDelete = view.findViewById(R.id.ivDelete)
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.view_item,parent,false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.cbTodo.text = "Sample Test"
+    }
+    override fun getItemCount(): Int {
+        return 1
+    }
+}
